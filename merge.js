9155ad20b1f9de677fd15638b689af213e1e8c5f
srijan9051 <srijanbanerjee18@gmail.com>
2024-05-16 11:49:21 +0530

    initial commit

---
+++ merge.js
@@ -0,0 +1,14 @@
+const PDFMerger = require('pdf-merger-js');
+
+var merger = new PDFMerger();
+
+const mergedPdf=async (p1,p2) => {
+  await merger.add(p1);  //merge all pages. parameter is the path to file and filename.
+  await merger.add(p2); // merge only page 2
+
+  let d=new Date().getTime()
+  await merger.save(`public/${d}.pdf`); //save under given name and reset the internal document
+  return d;
+}
+
+module.exports={mergedPdf}
