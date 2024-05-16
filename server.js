01eb4813971f32a8529fe6ad3aac83d0bc0ed5dc
srijan9051 <srijanbanerjee18@gmail.com>
2024-05-16 12:56:53 +0530

    fourth commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 app.post('/merge', upload.array('pdfs', 2), async (req, res, next) =>{
   
     let d=await mergedPdf(path.join(__dirname,req.files[0].path),path.join(__dirname,req.files[1].path))
-    res.redirect(`http://localhost:3000/static/${d}.pdf`)
+    res.redirect(`https://github.com/srijan9051/node-projects/blob/master/public/${d}.pdf`)
     // req.files is array of `photos` files
     // req.body will contain the text fields, if there were any
   })
